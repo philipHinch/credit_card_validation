d8eda37632d57c83b59f9a25ed90468b815af4eb
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-12 22:55:38 +0100

    installed react select and created form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 //packages
 import Tilt from 'react-tilt'
+//components
+import Form from './components/Form';
 //icons
 import { Icon } from '@iconify/react';
 //images
@@ -19,59 +21,66 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Tilt className="Tilt" options={{ max: 25 }}  >
-        <div className={`card ${ flipped && 'flipped' }`}>
-          <div className="content">
-            <div className="front" onClick={handleClick}>
-              <div className="imageOverlay">
-                <img src={ripples} alt="ripples" />
-              </div>
-              <div className="logoContainer">
-                <img src={logo} alt="logo" />
-              </div>
-              <div className="chipContainer">
-                <img src={chip} alt="chip image" />
-                <Icon className='contactlessIcon' icon="mdi:contactless-payment" />
-              </div>
-              <div className="cardNumber">
-                <p className="digits">4501</p>
-                <p className="digits">8835</p>
-                <p className="digits">0051</p>
-                <p className="digits">6377</p>
-              </div>
-              <div className="validContainer">
-                <div className="validLeft">
-                  <small className="validText">valid</small><small className="validText">thru</small>
-                  <Icon className='triangleIcon' icon="entypo:triangle-right" />
+    <>
+      <div className="container">
+        <div className="flipContainer">
+          <Icon className="flipIcon" icon="akar-icons:arrow-clockwise" />
+          <small className="flipText">Click Card</small>
+        </div>
+        <Tilt className="Tilt" options={{ max: 25 }}  >
+          <div className={`card ${ flipped && 'flipped' }`}>
+            <div className="content">
+              <div className="front" onClick={handleClick}>
+                <div className="imageOverlay">
+                  <img src={ripples} alt="ripples" />
                 </div>
-                <div className="validRight">
-                  <p className="digits validationDigits">05/24</p>
+                <div className="logoContainer">
+                  <img src={logo} alt="logo" />
+                </div>
+                <div className="chipContainer">
+                  <img src={chip} alt="chip image" />
+                  <Icon className='contactlessIcon' icon="mdi:contactless-payment" />
+                </div>
+                <div className="cardNumber">
+                  <p className="digits">4501</p>
+                  <p className="digits">8835</p>
+                  <p className="digits">0051</p>
+                  <p className="digits">6377</p>
+                </div>
+                <div className="validContainer">
+                  <div className="validLeft">
+                    <small className="validText">valid</small><small className="validText">thru</small>
+                    <Icon className='triangleIcon' icon="entypo:triangle-right" />
+                  </div>
+                  <div className="validRight">
+                    <p className="digits validationDigits">05/24</p>
+                  </div>
+                </div>
+                <div className="nameContainer">
+                  <p className="digits nameDigits">john doe</p>
+                </div>
+                <div className="visaIconContainer">
+                  <Icon className='visaIcon' icon="simple-icons:visa" />
+                  {/* <Icon className='visaIcon' icon="logos:mastercard" /> */}
                 </div>
               </div>
-              <div className="nameContainer">
-                <p className="digits nameDigits">john doe</p>
-              </div>
-              <div className="visaIconContainer">
-                <Icon className='visaIcon' icon="simple-icons:visa" />
-                {/* <Icon className='visaIcon' icon="logos:mastercard" /> */}
-              </div>
-            </div>
-            <div className="back" onClick={handleClick}>
-              <div className="magneticStrip"></div>
-              <div className="signatureContainer">
-                <div className="signature">John Doe</div>
-                <div className="cvv">734</div>
-              </div>
-              <div className="backText">
-                <h3>authorized signature</h3>
-                <p>If this card is found please send it to the bank. Misuse is criminal.</p>
+              <div className="back" onClick={handleClick}>
+                <div className="magneticStrip"></div>
+                <div className="signatureContainer">
+                  <div className="signature">John Doe</div>
+                  <div className="cvv">734</div>
+                </div>
+                <div className="backText">
+                  <h3>authorized signature</h3>
+                  <p>If this card is found please send it to the bank. Misuse is criminal.</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Tilt>
-    </div>
+        </Tilt>
+      </div>
+      <Form />
+    </>
 
 
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,59 @@
+//react select
+import Select from 'react-select';
+
+const titleOptions = [
+    { value: 'mr', label: 'Mr' },
+    { value: 'mrs', label: 'Mrs' },
+    { value: 'miss', label: 'Miss' }
+];
+
+const themeOptions = [
+    { value: 'ripples', label: 'Ripples' },
+    { value: 'fancy', label: 'Fancy' },
+    { value: 'triangles', label: 'Triangles' }
+]
+
+const backgroundOptions = [
+    { value: 'blue', label: 'Blue' },
+    { value: 'black', label: 'Black' },
+    { value: 'pink', label: 'Pink' },
+    { value: 'red', label: 'Red' }
+]
+
+const customStyles = {
+    option: (styles, state) => ({
+        ...styles,
+        cursor: 'pointer',
+    }),
+    control: (styles) => ({
+        ...styles,
+        cursor: 'pointer',
+    })
+}
+
+
 const Form = () => {
+
+
+
     return (
         <form>
-            form
+            <div className="titleContainer">
+                <label htmlFor="title">Title:</label>
+                <Select options={titleOptions} defaultValue={{ value: 'mr', label: 'Mr' }} styles={customStyles} />
+            </div>
+            <div className="nameInputContainer">
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" id="name" />
+            </div>
+            <div className="frontBackgroundContainer">
+                <label htmlFor="frontBackground">Front Theme:</label>
+                <Select options={themeOptions} defaultValue={{ value: 'ripples', label: 'Ripples' }} styles={customStyles} />
+            </div>
+            <div className="backBackgroundContainer">
+                <label htmlFor="backBackground">Back Color:</label>
+                <Select options={backgroundOptions} defaultValue={{ value: 'blue', label: 'Blue' }} styles={customStyles} />
+            </div>
         </form>
     );
 }
