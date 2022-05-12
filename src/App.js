03278a2341e45d4edc46cb49b30e12450524a140
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-12 15:47:20 +0100

    added 3d card and tilt

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+//packages
+import Tilt from 'react-tilt'
+//images
+import ripples from './assets/images/ripples.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <div className="card cardFront">
-        credit card
+    <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 250, width: 250 }} >
+      <div className="card">
+        <div className="content">
+          <div className="front">
+            <div className="imageOverlay">
+              <img src={ripples} alt="ripples" />
+            </div>
+            Front
+          </div>
+          <div className="back">
+            Back!
+          </div>
+        </div>
       </div>
-      <div className="card cardBack">
+    </Tilt>
 
-      </div>
-    </div>
+
   );
 }
 
